43a8a6fc7d72fb7bed55fbca04278c697d42a23d
Martin Eriksson <woodauncher+git@gmail.com>
2021-09-14 22:27:43 +0200

    Updated display_game and tested that it works.

--- src/graphics.py
+++ src/graphics.py
@@ -1,20 +1,34 @@
+from color import Color
+from dataclasses import dataclass
+from game_state import Player
+
+
 @dataclass
 class GraphicsHandler:
-    def display_game(self, board):
+    def display_game(self, board: [Color]):
+        # NOTE: Modifies board variable destructively (contents change outside function)
+        for idx, c in enumerate(board):
+            if c == Color.Black:
+                board[idx] = "■"
+            elif c == Color.White:
+                board[idx] = "o"
+            else:
+                board[idx] = " "
+
         print(
-            "[{}]1----------------[{}]2----------------[{}]3\n"
-            " |   \               |               /   |\n"
-            " |     [{}]4---------[{}]5---------[{}]6    |\n"
-            " |      |            |            |      |\n"
-            " |      |     [{}]7--[{}]8--[{}]9    |      |\n"
-            " |      |      |           |      |      |\n"
-            "[{}]10--[{}]11--[{}]12       [{}]13--[{}]14--[{}]15\n"
-            " |      |      |           |      |      |\n"
-            " |      |     [{}]16-[{}]17-[{}]18   |      |\n"
-            " |      |            |            |      |\n"
-            " |     [{}]19--------[{}]20--------[{}]21   |\n"
-            " |   /               |               \   |\n"
-            "[{}]22---------------[{}]23---------------[{}]24\n".format(
+            "[{}]1────────────────[{}]2────────────────[{}]3\n"
+            " │   \               │               /   │\n"
+            " │     [{}]4─────────[{}]5─────────[{}]6    │\n"
+            " │      │            │            │      │\n"
+            " │      │     [{}]7──[{}]8──[{}]9    │      │\n"
+            " │      │      │           │      │      │\n"
+            "[{}]10──[{}]11──[{}]12       [{}]13──[{}]14──[{}]15\n"
+            " │      │      │           │      │      │\n"
+            " │      │     [{}]16─[{}]17─[{}]18   │      │\n"
+            " │      │            │            │      │\n"
+            " │     [{}]19────────[{}]20────────[{}]21   │\n"
+            " │   /               │               \   │\n"
+            "[{}]22───────────────[{}]23───────────────[{}]24\n".format(
                 board[0], board[1], board[2],
                 board[3], board[4], board[5],
                 board[6], board[7], board[8],
@@ -24,12 +38,11 @@ class GraphicsHandler:
                 board[18], board[19], board[20],
                 board[21], board[22], board[23]))
 
-    def display_winner(self, player: str):
+    def display_winner(self, player: Player):
         print(
             "-----------------------------------------\n"
             "|                                       |\n"
             "|   {} has won the game!          |\n"
             "|                                       |\n"
             "|          Play again? y/n              |\n"
-            "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n".format(player)
-        )
+            "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n".format(player.name))
--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 import os
 
+import graphics
+from color import Color
+
 
 def main():
     name = os.name
     print(f"hello, world from OS='{name}'")
 
+    gh = graphics.GraphicsHandler()
+    gh.display_game([Color.White, Color.Black, Color.Empty] * 10)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
