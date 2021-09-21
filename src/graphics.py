82600608c5454739f686813f2c513c3279a74f01
Yu-Chuan Tsai <50803416+karta3426406@users.noreply.github.com>
2021-09-21 11:27:21 +0200

    add display status

--- src/graphics.py
+++ src/graphics.py
@@ -16,14 +16,19 @@ class GraphicsHandler:
         print("       >    MENU    < \n\n"
               "       -   Play [P] \n"
               "       -   Exit [E] \n")
-    def display_status(self, ):
-        # Title
+    def display_status(self, gs: GameState):
+        # Game title
         print("  ╔═════════════════════════════════════════════════════════════════════════════════════════════╗\n"
               "  ║                                           UU-Game                                           ║\n"
               "  ╚═════════════════════════════════════════════════════════════════════════════════════════════╝\n")
-        # Round
-        print("   Round %d ( remaining turns: %d ) \n" %(current_turn, 250-current_turn))
-        # Player status
+        # Player status        
+        print("   Round %d ( remaining turns: %d )" %(gs.current_turn, 250-gs.current_turn))
+        print("  ┌───────────────────────┬────────────────────┐\n"
+              "  │ %-15s( ██ ) │   %2d pieces left   │\n"
+              "  ├───────────────────────┼────────────────────┤\n"
+              "  │ %-15s( ░░ ) │   %2d pieces left   │\n" 
+              "  └───────────────────────┴────────────────────┘\n" %(gs.player1.name, gs.player1.coins_left_to_place, gs.player2.name, gs.player2.coins_left_to_place))
+        
 
 
     def display_game(self, board: [Color]):
@@ -72,29 +77,7 @@ class GraphicsHandler:
                 board[15], board[16], board[17],
                 board[18], board[19], board[20],
                 board[21], board[22], board[23]))
-        """
-        print(
-            "[{}]1────────────────[{}]2────────────────[{}]3\n"
-            " │   \               │               /   │\n"
-            " │     [{}]4─────────[{}]5─────────[{}]6    │\n"
-            " │      │            │            │      │\n"
-            " │      │     [{}]7──[{}]8──[{}]9    │      │\n"
-            " │      │      │           │      │      │\n"
-            "[{}]10──[{}]11──[{}]12       [{}]13──[{}]14──[{}]15\n"
-            " │      │      │           │      │      │\n"
-            " │      │     [{}]16─[{}]17─[{}]18   │      │\n"
-            " │      │            │            │      │\n"
-            " │     [{}]19────────[{}]20────────[{}]21   │\n"
-            " │   /               │               \   │\n"
-            "[{}]22───────────────[{}]23───────────────[{}]24\n".format(
-                board[0], board[1], board[2],
-                board[3], board[4], board[5],
-                board[6], board[7], board[8],
-                board[9], board[10], board[11],
-                board[12], board[13], board[14],
-                board[15], board[16], board[17],
-                board[18], board[19], board[20],
-                board[21], board[22], board[23]))"""
+
 
     def display_winner(self, player: Player):
         print(
