e3afeacdb8f7e8fe02f567d8dfbac44889333476
Yu-Chuan Tsai <50803416+karta3426406@users.noreply.github.com>
2021-09-21 09:40:44 +0200

    Add files via upload
    9/20

---
+++ color.py
@@ -0,0 +1,7 @@
+from enum import Enum, auto
+
+
+class Color(Enum):
+    Empty = auto()
+    White = auto()
+    Black = auto()
---
+++ input_handler.py
@@ -0,0 +1,4 @@
+class InputHandler:
+    def get_input(self, question: str) -> [str]:
+        response = input(question)
+        return response.replace(",", " ").split(" ")
