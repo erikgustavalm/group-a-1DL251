c270c3852a45dd8526db4024567c1d58c1171a8d
Yu-Chuan Tsai <50803416+karta3426406@users.noreply.github.com>
2021-09-21 11:26:22 +0200

    Simplified program
    move display status to graphic.py

--- src/main.py
+++ src/main.py
@@ -14,12 +14,12 @@ def main():
     
     game_start = True
     while game_start:
-    	gh.display_menu()
+        gh.display_menu()
     	
-    	menu_option = Input.get_input("   Option([ P / E ]):  ")
-    	option = menu_option[0].upper()
+        menu_option = Input.get_input("   Option([ P / E ]):  ")
+        option = menu_option[0].upper()
     	
-    	if option[0] == 'E':
+        if option[0] == 'E':
 	        while game_start:
         		sure_exit = Input.get_input("   Sure to exit([ Y / N ]):  ")
         		sure_exit = sure_exit[0].upper()
@@ -33,11 +33,9 @@ def main():
         		else:
         			print("   Invalid input !\n")
         			continue 
-    	elif option[0] == 'P':
+        elif option[0] == 'P':
         	os.system('clear')
-	        
-	        state = game_state.GameState()
-	        
+
 	        print("   Please input player name (Ｗithin 15 words):\n"
                   "   -------------------------------------------")
 	        p1_name = Input.get_input("   Player 1:  ")
@@ -47,27 +45,18 @@ def main():
 	        random.seed('foobar')
 	        seq = [ 1, 2 ]
 	        if random.choice(seq) == '1':
-	        	p_black = game_state.Player(str(p1_name), Color.Black, 11)
-	        	p_white = game_state.Player(p2_name[0], Color.White, 11)
+	        	p_black = game_state.Player( ' '.join(p1_name), Color.Black, 11 )
+	        	p_white = game_state.Player( ' '.join(p2_name), Color.White, 11 )
 	        else:
-	        	p_black = game_state.Player(p2_name[0], Color.Black, 11)
-	        	p_white = game_state.Player(p1_name[0], Color.White, 11)
+	        	p_black = game_state.Player( ' '.join(p2_name), Color.Black, 11 )
+	        	p_white = game_state.Player( ' '.join(p1_name), Color.White, 11 )
+            
+	        state = game_state.GameState( p_black, p_white, [Color.Empty]*24, 1, p_black )
             
 	        game_is_running = True
 	        while game_is_running:
-	        	# gh.display_status()
-	        	# Title
-		        print("  ╔═════════════════════════════════════════════════════════════════════════════════════════════╗\n"
-		              "  ║                                           UU-Game                                           ║\n"
-		              "  ╚═════════════════════════════════════════════════════════════════════════════════════════════╝\n")
-		        
-		        print("   Round %d ( remaining turns: %d )" %(state.current_turn, 250-state.current_turn))
-		        print("  ┌───────────────────────┬────────────────────┐\n"
-		        	  "  │ %-15s( ██ ) │   %2d pieces left   │\n"
-		        	  "  ├───────────────────────┼────────────────────┤\n"
-		        	  "  │ %-15s( ░░ ) │   %2d pieces left   │\n" 
-		        	  "  └───────────────────────┴────────────────────┘\n" %(p_black.name, p_black.coins_left_to_place, p_white.name, p_white.coins_left_to_place))
-		        gh.display_game([Color.White, Color.Black, Color.Empty] * 8)
+	        	gh.display_status(state)
+		        gh.display_game( state.board )
 
 		        # Command action
                 # Rules
@@ -76,7 +65,7 @@ def main():
                 # end_game situation
 		        game_is_running = False          
 		        game_start = False
-    	else:
+        else:
 	        print("  Invalid input !\n ")
 	        continue
     
