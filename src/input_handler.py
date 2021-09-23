9d2a48151885d93a6acbeaf63afa87dcadadd8b1
Martin-Eriksson <34482520+Martin-Eriksson@users.noreply.github.com>
2021-09-23 20:04:45 +0200

    Revert "Revert "Implemented game to an acceptable level.""

---
+++ src/input_handler.py
@@ -0,0 +1,10 @@
+class InputHandler:
+    def get_input(self, question: str) -> str:
+        return input(question).upper()
+
+    def get_input_keep_case(self, question: str) -> str:
+        return input(question)
+
+    def get_separated_input(self, question: str) -> [str]:
+        response = input(question).upper()
+        return list(filter(None, response.replace(",", " ").split()))
